25ea885e7cc9e3da54584a94fff282c7bd02cfad
Ata Ur Rehman Malik <119836227+arkhan998@users.noreply.github.com>
2024-05-27 23:17:06 +0500

    Update Malik.js

--- commands/Malik.js
+++ commands/Malik.js
@@ -1,22 +1,23 @@
-const { tlang, cmd } = require('../lib');
+const { tlang,cmd } = require('../lib')
 
-const Config = require('../config');
+const Config = require('../config')
 
-const axios = require('axios');
+const axios = require('axios')
 
-const url = 'https://gist.github.com/arkhan998/82e85e344457386b0c957c770e55d125/raw';
 
-cmd({ on: 'text' }, async (Void, citel, text, { isCreator }) => {
-  let { data } = await axios.get(url);
 
-  // Check for variations of "Hi" (case-insensitive)
-  const greetings = ['hi', 'hey', 'hello']; // Add more variations if needed
-  if (greetings.some(greeting => text.toLowerCase().includes(greeting))) {
-    // Send the first audio clip (assuming "Hi" audio)
-    return Void.sendMessage(citel.chat, {
-      audio: { url: data[Object.keys(data)[0]] }, // Access first audio clip
-      mimetype: 'audio/mpeg',
-      ptt: true,
-    }, { quoted: citel });
-  }
-});
+const url = 'https://gist.github.com/arkhan998/82e85e344457386b0c957c770e55d125/raw'
+
+
+
+cmd({ on: "text" }, async (Void,citel,text,{isCreator})=> {
+
+  let { data } = await axios.get(url)
+
+  for (vr in data){
+
+ if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:citel})   
+
+}
+
+})
